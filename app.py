4a1a31b034d2326f230aa10efb132211a7afc465
Gabriel Blanchet-Poitras <gabrielbpoitras@gmail.com>
2025-07-07 15:36:46 -0400

    Implemented a metaclass that streamlines database queries throught ORM abstraction. Implemented an SQLalchemy ORM .

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask,  jsonify, request, make_response
 from flask_restful import Api, Resource
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
-from flask_sqlalchemy import SQLAlchemy
 
-from sqlalchemy import  text
 from werkzeug.security import generate_password_hash, check_password_hash
-
-
 import secrets
 import datetime
 import os
+
+from models import Users, UserApi, CryptoApi, db
 
 
 ## KEYS AND CONFIG
@@ -36,22 +34,17 @@ app.config["JWT_SECRET_KEY"] = f"{secure_key}"
 
 ### APP INIT
 
-db = SQLAlchemy(app)
+
 api = Api(app)
 jwt = JWTManager(app)
 CORS(app, supports_credentials=True)
 
 
 ### DATABASE INIT
+db.init_app(app)
 
 with app.app_context():
-
-
-      db.session.execute(text("""CREATE TABLE IF NOT EXISTS 
-                              users (id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, 
-                              email VARCHAR(50), password VARCHAR(256), username VARCHAR(30))"""))
-      db.session.commit()
-        
+ db.create_all()
 
 
 ### RESSOURCES 
@@ -61,8 +54,8 @@ class User_data(Resource):
    def get(self):
       identity = get_jwt_identity()
 
-      stmt = text("""SELECT username, email, password FROM users WHERE username = :username""")
-      user_data = db.session.execute(stmt, {'username': identity}).fetchone()
+      api = UserApi()
+      user_data = api.get_by_username(identity)
 
       if user_data:
         username = user_data.username 
@@ -72,60 +65,23 @@ class User_data(Resource):
       else:
          return {'message': 'Error'}, 404
 
-class Password_change(Resource):
-   @jwt_required()
-   def post(self):
-      identity = get_jwt_identity()
-
-      data = request.get_json()
-      new_password = data['new_password']
-      old_password = data['old_password']
-
-      stmt = text("""SELECT username, password FROM users WHERE username = :username""")
-      user_data = db.session.execute(stmt, {'username': identity}).fetchone()
-
-      if check_password_hash(user_data.password, old_password):
-
-         update = text("UPDATE users SET password = :new_password WHERE username = :username")
-         db.session.execute(update, {'new_password': generate_password_hash(new_password), 'username': identity})
-         db.session.commit()
-         
-         response = make_response(jsonify({'message': 'Password updated !'}))
-         return response
-      
-      else:
-         response = make_response(jsonify({'Error': "Your password is incorrect"})), 404
-         return response
 
 
 class Btc(Resource):
   
   def get(self):
-
-    rows = db.session.execute(text("SELECT * FROM bitcoin_data")).mappings().all()
-    clean = [dict(row) for row in rows]
+    api = CryptoApi()
+    rows = api.get_all()
+    clean = [row.serialize() for row in rows]
     return jsonify({"content": clean})
 
 
 api.add_resource(User_data, '/api/users/')
 api.add_resource(Btc, '/api/bitcoin')
-api.add_resource(Password_change, '/change/password' )
+
 
 
 ### ROUTES
-
-@app.route('/debug', methods=['GET'])
-@jwt_required()
-def debug_auth():
-    auth_header = request.headers.get("Authorization", "").strip()
-    current_user = get_jwt_identity()
-    print("Received Header:", auth_header)
-    print("Extracted User from JWT:", current_user)
-    return jsonify({
-        "received_header": auth_header,
-        "extracted_user": current_user
-    })
-
 
 @app.route('/api/sign-up', methods=['POST'])
 def register():
@@ -142,9 +98,8 @@ def register():
        return jsonify({"message": "Username and password required"}), 400
     
     hashed_password = generate_password_hash(password)
-    
-    stmt = text("INSERT INTO users (email, password, username) VALUES (:email, :password, :username)")
-    db.session.execute(stmt, {"email": email, "password": hashed_password, "username": username})
+    new_user = Users(username=username, email=email, password=hashed_password)
+    db.session.add(new_user)
     db.session.commit()
     
     response = make_response(jsonify({'message': 'Registration complete'}))
@@ -162,22 +117,21 @@ def authenticate():
    try:
 
     data = request.get_json()
-    username = data['username']
+    identity = data['username']
     password = data['password']
+
+    api = UserApi()
+    user_data = api.get_by_username(identity)
     
-    smtm = text((f"SELECT password FROM users WHERE username = :username"))
-    query = db.session.execute(smtm, {"username": username}).fetchone()
-    
-    
-    if not query:
+    if not user_data:
        
        return jsonify({"message": "User not found"}), 404
     
-    hashed_password = query.password
+    hashed_password = user_data.password
     if check_password_hash(hashed_password, password):
        
        
-       access_token = create_access_token(identity=username, expires_delta=exp)
+       access_token = create_access_token(identity=identity, expires_delta=exp)
      
        message = {"message": "Login successful!", "access_token": access_token}
        response = make_response(jsonify(message))
@@ -189,6 +143,31 @@ def authenticate():
    except Exception as e:
        return {"message": "An error occured", 'error': str(e)}, 500
    
+
+
+@app.route('/change/password', methods=['POST'])
+@jwt_required()
+def post():
+
+   identity = get_jwt_identity()
+   data = request.get_json()
+   new_password = data['new_password']
+   old_password = data['old_password']
+
+   api = UserApi()
+   user_data = api.get_by_username(identity)
+
+   if user_data and check_password_hash(user_data.password, old_password):
+
+      user_data.password = generate_password_hash(new_password)
+      db.session.commit()
+      
+      response = make_response(jsonify({'message': 'Password updated !'}))
+      return response
+   
+   else:
+      response = make_response(jsonify({'Error': "Your password is incorrect"})), 404
+      return response
 
 
 ### MAIN APP
---
+++ models.py
@@ -0,0 +1,81 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+class QueryMeta(type):
+    def __new__(cls, name, bases, dict):
+        if 'model' in dict:
+            model = dict['model']
+        
+            def get_by_username(self, identity):
+                user_data = db.session.query(model).filter_by(username = identity).first()
+                return user_data
+            
+            def get_all(self):
+                all_data = db.session.query(model).all()
+                return all_data
+                
+            dict['get_by_username'] = get_by_username
+            dict['get_all'] = get_all
+
+        return super().__new__(cls, name, bases, dict)
+    
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+
+
+class Bitcoin_data(db.Model):
+    date = db.Column(db.DateTime(timezone=True), primary_key=True, index=True) 
+    price = db.Column(db.Numeric(20,2)) 
+    volume = db.Column(db.Numeric(20,2)) 
+    marketcap = db.Column(db.Numeric(20,2)) 
+    availablesupply = db.Column(db.Numeric(20,2)) 
+    totalsupply = db.Column(db.Integer) 
+    fullydilutedvaluation = db.Column(db.Numeric(20,2)) 
+    pricechange1h = db.Column(db.Numeric(20,2)) 
+    pricechange1d = db.Column(db.Numeric(20,2)) 
+    pricechange1w = db.Column(db.Numeric(20,2))
+
+    def serialize(self):
+     
+     return {
+        "date": self.date.isoformat() if self.date else None,
+        "price": float(self.price) if self.price else None,
+    }
+
+
+class Eth_data(db.Model):
+    date = db.Column(db.DateTime(timezone=True), primary_key=True, index=True) 
+    price = db.Column(db.Numeric(20,2)) 
+    volume = db.Column(db.Numeric(20,2)) 
+    marketcap = db.Column(db.Numeric(20,2)) 
+    availablesupply = db.Column(db.Numeric(20,2)) 
+    totalsupply = db.Column(db.Integer) 
+    fullydilutedvaluation = db.Column(db.Numeric(20,2)) 
+    pricechange1h = db.Column(db.Numeric(20,2)) 
+    pricechange1d = db.Column(db.Numeric(20,2)) 
+    pricechange1w = db.Column(db.Numeric(20,2))
+
+
+class Market_data(db.Model):
+    date = db.Column(db.DateTime(timezone=True), primary_key=True, index=True) 
+    volume = db.Column(db.BigInteger) 
+    marketcap = db.Column(db.BigInteger) 
+    btcdominance = db.Column(db.Numeric(20,2))
+    marketcapchange = db.Column(db.Numeric(20,2))
+    volumechange = db.Column(db.Numeric(20,2))
+    btcdominancechange = db.Column(db.Numeric(20,2))
+    fear_greed_value = db.Column(db.Integer)
+    fear_greed_name = db.Column(db.String(20))
+
+
+class UserApi(metaclass=QueryMeta):
+    model = Users
+
+class CryptoApi(metaclass=QueryMeta):
+    model = Bitcoin_data
+
